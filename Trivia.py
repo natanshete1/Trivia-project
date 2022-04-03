80c6d07e6a293151bf9ed64a7ef7d20902ce9131
yalem <yalem8@gmail.com>
2022-04-03 22:28:50 +0300

    commit user qutions work

--- Trivia.py
+++ Trivia.py
@@ -11,15 +11,15 @@ n = len(qus)
 score = 0
 print(ans2_list)
 for i in range(n):
-    print(qus[i])
-    print(f"a = {ans[i][0]}\nb = {ans[i][1]}\nc = {ans[i][2]}\nd = {ans[i][3]}")
-    guess = input("Enter answer :")
-    while guess not in ("a", "b", "c", "d"):
+    print(qus[i]) # present qustion
+    print(f"a = {ans[i][0]}\nb = {ans[i][1]}\nc = {ans[i][2]}\nd = {ans[i][3]}") # show options
+    guess = input("Enter answer :") # get player input
+    while guess not in ("a", "b", "c", "d"): # check valid input
         print("Try Again(a,b,c,d)")
         print(qus[i])
         print(f"a = {ans[i][0]}\nb = {ans[i][1]}\nc = {ans[i][2]}\nd = {ans[i][3]}")
         guess = input("Enter answer :")
-    if guess == ans2_list[i] :
+    if guess == ans2_list[i] : # Compar if answer is correct
         score += 9
         print("Right answer\n*************************")
     else:
@@ -28,4 +28,7 @@ for i in range(n):
         print("Round score = ", score)
         print("**********************************\n")
         print("***************Next Round********************")
+    if i == 9 and guess == ans2_list[i] :
+        score +=9
+
 print("Final score = ", score)
