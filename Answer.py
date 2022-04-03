2faffb6524c54cd540bf446cbaab3b6f51060e3b
yalem <yalem8@gmail.com>
2022-04-03 16:38:26 +0300

    commit 3

---
+++ Answer.py
@@ -0,0 +1,29 @@
+a_options = ["China",
+             "Japan",
+             "Italy",
+             "Korea"
+             ]
+
+b_options = ["Moses",
+             "Noah",
+             "Japheth",
+             "Jacob"
+             ]
+
+c_options = ["Albert Einstein",
+             "Isaac Newton",
+             "Stephen Hawking",
+             "Glilau Galilei"
+             ]
+
+d_options = ["Stove",
+             "light bulb",
+             "car",
+             "calculator"
+             ]
+
+e_options = ["Aphrodite",
+             "Deimos",
+             "Pantheon",
+             "Phobos"
+             ]
--- Question.py
+++ Question.py
@@ -1,13 +1,10 @@
-# What Did Thomas Edison Invent?
+a= "Which country do you drink sake in?"
 #
+b = "In the Bible who built the ark"
 #
+c = "Who discovered gravity?"
 #
+d = "What Did Thomas Edison Invent?"
 #
-#
-#
-#
-#
-#
-#
-#
+e = "The goddess of love in Greek mythology is called...?"
 #
