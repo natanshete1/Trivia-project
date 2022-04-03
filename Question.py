ef1a485bfaf4604909ebd4b768343291d12b57bb
yalem <yalem8@gmail.com>
2022-04-03 17:13:43 +0300

    commit all questions

--- Question.py
+++ Question.py
@@ -8,3 +8,12 @@ d = "What Did Thomas Edison Invent?"
 #
 e = "The goddess of love in Greek mythology is called...?"
 #
+f_="Question=Where in the US is Hollywood?"
+#
+g_="Question=Where is the Knesset of Israel?"
+#
+h= "What is called grape picking?"
+#
+I="Which of the following is not a fruit?"
+#
+Bonos = "Which of the following is not a type of wine?"
