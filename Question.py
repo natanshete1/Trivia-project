db197729171a1e3b02021713c813a70bd156763a
yalem <yalem8@gmail.com>
2022-04-03 16:57:11 +0300

    commit all questions

--- Question.py
+++ Question.py
@@ -1,13 +1,10 @@
-
-f_="Question=Where in the US is Hollywood?"
-g_="Question=Where is the Knesset of Israel?"
-H= "What is called grape picking?"
-I="Which of the following is not a fruit?"
-f = "Which of the following is not a type of wine?"
+a= "Which country do you drink sake in?"
 #
+b = "In the Bible who built the ark"
 #
+c = "Who discovered gravity?"
 #
+d = "What Did Thomas Edison Invent?"
 #
-#
-#
+e = "The goddess of love in Greek mythology is called...?"
 #
