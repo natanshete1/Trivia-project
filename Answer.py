0b64a72a2b5b9c494c32db1cddce21a765143cf7
natan <natanshete1@gmail.com>
2022-04-03 16:37:04 +0300

    commit 2

---
+++ Answer.py
@@ -0,0 +1,25 @@
+#
+f_Answer = ["California",""
+            "Los Angeles",
+            "New York",
+            "Las Vegas"]
+
+g_Answer =["Jerusalem",
+           "Tel Aviv",
+           "Hadera",
+           "Herzliya"]
+
+H_Answer = ["vintage",
+            "olive harvest",
+            "date harvest",
+            "picking"]
+
+I_Answer = ["Persian",
+            "Banana",
+            "Parsley",
+            "Plum"]
+
+j_bonos = ["Jordan Katzrin",
+           "Chardonnay",
+           "Cabernet Franc",
+           "Bourbon"]
--- Question.py
+++ Question.py
@@ -1,9 +1,9 @@
-# What Did Thomas Edison Invent?
-#
-#
-#
-#
-#
+
+f_="Question=Where in the US is Hollywood?"
+g_="Question=Where is the Knesset of Israel?"
+H= "What is called grape picking?"
+I="Which of the following is not a fruit?"
+f = "Which of the following is not a type of wine?"
 #
 #
 #
